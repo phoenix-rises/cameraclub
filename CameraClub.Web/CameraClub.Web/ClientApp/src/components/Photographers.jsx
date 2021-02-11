f86c0b9e1e7cae6e5335c434ef80d97f15e9f7bc
phoenix-rises <phoenix-rises@users.noreply.github.com>
2021-02-11 17:43:03 -0600

    added editing page for photographers

--- CameraClub.Web/CameraClub.Web/ClientApp/src/components/Photographers.jsx
+++ CameraClub.Web/CameraClub.Web/ClientApp/src/components/Photographers.jsx
@@ -1,12 +1,201 @@
 ﻿import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { PhotographersModal } from './PhotographersModal';
 
 export class Photographers extends Component {
     static displayName = Photographers.name;
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            photographerData: [],
+            error: false,
+            loading: true,
+            errorMessage: "",
+            isModalVisible: false
+        }
+
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    componentDidMount() {
+        this.getPhotographerData();
+    }
+
+    showModal = (photographer) => {
+        if (photographer === null) {
+            photographer = { "id": null, "firstName": "", "lastName": "", "competitionNumber": "", "email": "", "clubNumber": "" };
+        }
+
+        this.setState(
+            {
+                isModalVisible: true,
+                currentPhotographer: photographer
+            });
+    };
+
+    hideModal = () => {
+        this.setState({ isModalVisible: false });
+    };
+
+    showError(error) {
+        console.log(error);
+        this.setState({
+            loading: false,
+            error: true,
+            errorMessage: error,
+            userApprovals: null
+        });
+    }
+
+    handleSave = (photographer) => {
+        var url = process.env.REACT_APP_API_URL + "UpsertPhotographer";
+
+        if (photographer.id !== null) {
+            fetch(url,
+                {
+                    method: "PUT",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        id: photographer.id,
+                        firstName: photographer.firstName,
+                        lastName: photographer.lastName,
+                        competitionNumber: photographer.competitionNumber,
+                        email: photographer.email,
+                        clubNumber: photographer.clubNumber
+                    })
+                })
+                .then(
+                    (result) => {
+                        var photographerToUpdate = this.state.photographerData.find(c => c.id === photographer.id);
+                        photographerToUpdate.firstName = photographer.firstName;
+                        photographerToUpdate.lastName = photographer.lastName;
+                        photographerToUpdate.email = photographer.email;
+                        photographerToUpdate.competitionNumber = photographer.competitionNumber;
+                        photographerToUpdate.clubNumber = photographer.clubNumber;
+
+                        this.hideModal();
+                    },
+                    (error) => {
+                        this.hideModal();
+                        this.showError(error);
+                    }
+                );
+        }
+        else {
+            fetch(url,
+                {
+                    method: "POST",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        firstName: photographer.firstName,
+                        lastName: photographer.lastName,
+                        competitionNumber: photographer.competitionNumber,
+                        email: photographer.email,
+                        clubNumber: photographer.clubNumber
+                    })
+                })
+                .then(
+                    (result) => {
+                        this.state.photographerData.push(photographer);
+
+                        this.hideModal();
+                    },
+                    (error) => {
+                        this.hideModal();
+                        this.showError(error);
+                    }
+                );
+        }
+    }
+
+    getPhotographerData() {
+        var url = process.env.REACT_APP_API_URL + 'GetPhotographers';
+
+        fetch(url,
+            {
+                method: "GET"
+            })
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        loading: false,
+                        error: false,
+                        errorMessage: null,
+                        photographerData: result,
+                        currentPhotographer: result[0]
+                    })
+                },
+                (error) => {
+                    this.showError(error);
+                }
+            );
+    }
+
+    renderPhotographers() {
+        return (
+            <>
+                <Row>
+                    <Col>
+                        <h1 className="page-title">Photographers</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="text-right">
+                        <button className="btn btn-primary" onClick={(e) => { e.preventDefault(); this.showModal(null); }}>Add Photographer</button>
+                    </Col>
+                </Row>
+                <Row>
+                    {this.state.photographerData.map(photographer =>
+                        <Container key={photographer.id} className="bs-callout bs-callout-info">
+                            <Row>
+                                <Col>
+                                    <h4 className="info">{photographer.firstName + " " + photographer.lastName}</h4>
+                                </Col>
+                            </Row>
+                            <Row className="top-margin-spacing">
+                                <Col sm={3}>Email</Col>
+                                <Col sm={9}><a href={"mailto:" + photographer.email}>{photographer.email}</a></Col>
+                            </Row>
+                            <Row>
+                                <Col sm={3}>Competition Number</Col>
+                                <Col sm={9}>{photographer.competitionNumber}</Col>
+                            </Row>
+                            <Row>
+                                <Col sm={3}>Club Number</Col>
+                                <Col sm={9}>{photographer.clubNumber}</Col>
+                            </Row>
+                            <Row className="top-margin-spacing">
+                                <Col>
+                                    <button className="btn btn-link" onClick={(e) => { e.preventDefault(); this.showModal(photographer); }}>Edit</button>
+                                </Col>
+                            </Row>
+                        </Container>
+                    )}
+                </Row>
+                <PhotographersModal handleClose={this.hideModal} handleSave={this.handleSave} show={this.state.isModalVisible} photographerData={this.state.currentPhotographer} />
+            </>
+        );
+    }
+
+
     render() {
+        let contents = this.state.error
+            ? <p>Error:  <span dangerouslySetInnerHTML={{ __html: this.state.errorMessage }}></span></p>
+            : this.state.loading
+                ? <p><em>Loading...</em></p>
+                : this.renderPhotographers();
+
         return (
             <div>
-                <p>TODO: this is the photographers page</p>
+                {contents}
             </div>
         );
     }
---
+++ CameraClub.Web/CameraClub.Web/ClientApp/src/components/PhotographersModal.jsx
@@ -0,0 +1,101 @@
+﻿import React, { Component } from 'react';
+import { Container, Row, Col, Label, Input } from 'reactstrap';
+
+export class PhotographersModal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { "id": null, "firstName": "", "lastName": "", "competitionNumber": "", "email": "", "clubNumber": "" };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSave() {
+        this.props.handleSave(this.state);
+    }
+
+    handleClose() {
+        this.props.handleClose();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (!prevProps.show && this.props.show) {
+            var data = this.props.photographerData;
+
+            this.setState(
+                {
+                    id: data.id,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    competitionNumber: data.competitionNumber,
+                    email: data.email,
+                    clubNumber: data.clubNumber
+                });
+        }
+    }
+
+    render() {
+        return (
+            <div className={this.props.show ? "modal display-block" : "modal display-none"}>
+                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{this.state.id === null ? "Add Photographer" : "Edit Photographer"}</h5>
+                            <button type="button" className="close" aria-label="Close" onClick={this.props.handleClose}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <Container className="modal-body">
+                            <Row>
+                                <Label for="firstName" sm={4}>First Name</Label>
+                                <Col sm={8}>
+                                    <Input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Label for="lastName" sm={4}>Last Name</Label>
+                                <Col sm={8}>
+                                    <Input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Label for="email" sm={4}>Email</Label>
+                                <Col sm={8}>
+                                    <Input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Label for="competitionNumber" sm={4}>Competition Number</Label>
+                                <Col sm={8}>
+                                    <Input type="text" name="competitionNumber" value={this.state.competitionNumber} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Label for="clubNumber" sm={4}>Club Number</Label>
+                                <Col sm={8}>
+                                    <Input type="text" name="clubNumber" value={this.state.clubNumber} onChange={this.handleChange} />
+                                </Col>
+                            </Row>
+                        </Container>
+                        <div className="modal-footer">
+                            <button className="btn btn-secondary" onClick={this.handleClose}>Close</button>
+                            <button className="btn btn-primary" onClick={this.handleSave}>Save Changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
