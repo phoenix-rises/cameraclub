9bd2921b047ef188e2882aa66669eadbf79bd189
Lyle Ormsby <phoenix-rises@outlook.com>
2022-12-26 19:09:58 -0600

    Added SQL to initiate club

---
+++ SQL/InitiateClub.sql
@@ -0,0 +1,4 @@
+INSERT INTO competition.Club
+    (Name, ContactName, ContactEmail, ClubAssociationNumber)
+VALUES
+    ('Champaign County Camera Club', 'Kim Ormsby', 'kim.d.ormsby@gmail.com', 'Unknown');
